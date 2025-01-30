4094edc5843fe06693b58bf8bb8d83e19ad19a57
ByeonginCho <cbicit@kookmin.ac.kr>
2025-01-30 14:38:23 +0900

    dd

--- problem1000.py
+++ problem1000.py
@@ -5,8 +5,4 @@ print(int(a)+int(b)) # input함수는 string으로 받기 때문에 int() 하기
 a, b = map(int, input().split())  # 입력 받은 값을 정수로 변환
 print(a + b)
 
-<<<<<<< HEAD
-#remote origin practice local_branch
-=======
-#remote origin practice main
->>>>>>> 1aca30a78bc6947cc64a65b4b162e625e60ab307
+#remote origin practice 
