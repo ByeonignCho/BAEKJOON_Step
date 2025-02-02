b5d3b29d7447139be48dedc8bb8b9cac1377145f
ByeonginCho <cbicit@kookmin.ac.kr>
2025-02-02 17:02:57 +0900

    11021

--- 27problem11021.py
+++ 27problem11021.py
@@ -1,3 +1,4 @@
+"""
 a = int(input())
 ans = []
 for i in range(a):
@@ -6,3 +7,13 @@ for i in range(a):
 
 for i in range(len(ans)): 
     print("Case #{}: {}".format(i+1, ans[i]))
+"""
+import sys
+num = sys.stdin.readline().rstrip()
+ans = []
+for i in range(int(num)):
+    a = sys.stdin.readline().rstrip()
+    n = a.split()
+    ans.append(int(n[0])+int(n[1]))
+for i in range(int(num)):
+    sys.stdout.write("Case #{}: {}\n".format(i+1, ans[i]))
